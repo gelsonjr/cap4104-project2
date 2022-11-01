58d4baedc2773dc985a72f3cecfdb6fe09862919
gelsonjr <gelsonjr@gmail.com>
2022-11-01 09:28:43 -0400

    dataframe working

--- apps/home.py
+++ apps/home.py
@@ -18,9 +18,12 @@ def app():
     with open("apps/clean_jason.json", "r") as jf:
         json_file = json.load(jf)
 
+    # adds data to dataframe, organizes, removes unwanted columns, reorder, etc
     json_data = pd.json_normalize(json_file) # organizes data 
-    json_data.drop(json_data.columns[[0, 3, 4, 5, 6, 7, 8]], axis=1, inplace=True) # removes unwanted columns
-    df = json_data[json_data.columns[[2, 1, 0]]] # reorder columns
+    pd.set_option('display.precision', 2)
+    #pd.options.display.float_format = '{:,.2f}'.format
+    json_data.drop(json_data.columns[[0, 3, 4, 5, 6, 7, 8]], axis=1, inplace=True)
+    df = json_data[json_data.columns[[2, 1, 0]]]
     df = df.rename(
         columns={
             'country.value' : 'Country',
@@ -28,10 +31,11 @@ def app():
             'date' : 'Year'
         }
     )
+    df = df.sort_values(by='Population', ascending=False, ignore_index=True)[:10]
+    df['Population'] = df['Population'].astype(float)
+    df.index += 1
 
-    df = df.sort_values(by='value', ascending=False)
-
-    st.write(df)
+    st.dataframe(df.style.format({'Population': '{:,.2f}'}))
 
     st.write('Navigate to `Data Stats` page to visualize the data')
 
