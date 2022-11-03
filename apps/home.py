a208f1b926fa94a9b130b950c9613e0c4f043422
gelsonjr <gelsonjr@gmail.com>
2022-11-03 12:44:30 -0400

    pushing lastest I have

--- apps/home.py
+++ apps/home.py
@@ -5,6 +5,27 @@ from data.create_data import create_table
 
 import json
 
+def fetch_api_data(url):
+        # with open("apps/jason2021.json", "r") as jf:
+        with open(url, "r") as jf:
+            json_file = json.load(jf)
+        json_data = pd.json_normalize(json_file[1][49:])
+        json_data.drop(json_data.columns[[0, 3, 4, 5, 6, 7, 8]], axis=1, inplace=True)
+        df = json_data[json_data.columns[[2, 1, 0]]]
+        df = df.rename(
+            columns={
+                'country.value' : 'Country',
+                'value' : 'Population',
+                'date' : 'Year'
+            }
+        )
+        df = df.sort_values(by='Population', ascending=False, ignore_index=True)[:10]
+        df['Population'] = df['Population'].astype(float)
+        df.index += 1
+        df = df.style.format({'Population': '{:,.2f}'})
+        return df
+
+
 def app():
     st.title('Home')
 
@@ -17,27 +38,37 @@ def app():
     # with open("apps/clean_jason.json", "r") as jf:
     #     json_file = json.load(jf)
 
-    with open("apps/jason2021.json", "r") as jf:
-        json_file = json.load(jf)
+
+
+    # with open("apps/jason2021.json", "r") as jf:
+    #     json_file = json.load(jf)
     
-    # st.dataframe(json_file[1])
+    # # adds data to dataframe, organizes, removes unwanted columns, reorder, etc
+    # json_data = pd.json_normalize(json_file[1][49:])
+    # json_data.drop(json_data.columns[[0, 3, 4, 5, 6, 7, 8]], axis=1, inplace=True)
+    # df = json_data[json_data.columns[[2, 1, 0]]]
+    # df = df.rename(
+    #     columns={
+    #         'country.value' : 'Country',
+    #         'value' : 'Population',
+    #         'date' : 'Year'
+    #     }
+    # )
+    # df = df.sort_values(by='Population', ascending=False, ignore_index=True)[:10]
+    # df['Population'] = df['Population'].astype(float)
+    # df.index += 1
+    # df = df.style.format({'Population': '{:,.2f}'})
 
-    # adds data to dataframe, organizes, removes unwanted columns, reorder, etc
-    json_data = pd.json_normalize(json_file[1][49:])
-    json_data.drop(json_data.columns[[0, 3, 4, 5, 6, 7, 8]], axis=1, inplace=True)
-    df = json_data[json_data.columns[[2, 1, 0]]]
-    df = df.rename(
-        columns={
-            'country.value' : 'Country',
-            'value' : 'Population',
-            'date' : 'Year'
-        }
-    )
-    df = df.sort_values(by='Population', ascending=False, ignore_index=True)[:10]
-    df['Population'] = df['Population'].astype(float)
-    df.index += 1
 
-    st.dataframe(df.style.format({'Population': '{:,.2f}'}))
+    url2021 = 'apps/jason2021.json'
+    url2020 = 'apps/jason2020.json'
+    df2021 = fetch_api_data(url2021)
+    df2020 = fetch_api_data(url2020)
+
+
+
+    st.dataframe(df2021)
+    st.dataframe(df2020)
 
     #st.write('Navigate to `Data Stats` page to visualize the data')
 
